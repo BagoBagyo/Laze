c0a6b88825d6979f0cc12e0a21b7044a7195dd9b
Cesar Managad <cesar.man@sbcglobal.net>
2015-01-26 00:59:38 -0800

    Initial

---
+++ app/src/main/java/com/example/cesar/laze/Block.java
@@ -0,0 +1,59 @@
+package com.example.cesar.laze;
+
+import java.util.List;
+
+/**
+ * Created by Cesar on 1/24/2015.
+ * Basic Block class.
+ */
+public class Block {
+    enum Type {
+        OPEN, MIRROR, GLASS, CRYSTAL, WORMHOLE, BLACKHOLE
+    }
+
+    private Type type;
+    private List<Integer> northFace;
+    private List<Integer> eastFace;
+    private List<Integer> southFace;
+    private List<Integer> westFace;
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public List<Integer> getNorthFace() {
+        return northFace;
+    }
+
+    public void setNorthFace(List<Integer> northFace) {
+        this.northFace = northFace;
+    }
+
+    public List<Integer> getEastFace() {
+        return eastFace;
+    }
+
+    public void setEastFace(List<Integer> eastFace) {
+        this.eastFace = eastFace;
+    }
+
+    public List<Integer> getSouthFace() {
+        return southFace;
+    }
+
+    public void setSouthFace(List<Integer> southFace) {
+        this.southFace = southFace;
+    }
+
+    public List<Integer> getWestFace() {
+        return westFace;
+    }
+
+    public void setWestFace(List<Integer> westFace) {
+        this.westFace = westFace;
+    }
+}
---
+++ app/src/main/java/com/example/cesar/laze/Ray.java
@@ -0,0 +1,36 @@
+package com.example.cesar.laze;
+
+/**
+ * Created by Cesar on 1/24/2015.
+ * Basic Ray object
+ */
+public class Ray {
+    private int x;
+    private int y;
+    private int direction;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+}
+
