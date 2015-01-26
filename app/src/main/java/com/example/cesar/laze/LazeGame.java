28ac52fa93b280388cc678b033b978bcf5b0ad71
Cesar Managad <cesar.man@sbcglobal.net>
2015-01-26 01:38:57 -0800

    Starting LazeGame.

---
+++ app/src/main/java/com/example/cesar/laze/LazeGame.java
@@ -0,0 +1,16 @@
+package com.example.cesar.laze;
+
+import java.util.List;
+
+/**
+ * Created by Cesar on 1/26/2015.
+ * LazeGame Model class
+ */
+public class LazeGame {
+    //Block[][] play = new Block[2][2];
+
+    //play[0][0] = new Block(Block.Type.O);
+
+    Block myBlock = new Block(Block.Type.OPEN, new List<Integer>(0), new List<Integer>(0), new List<Integer>(0), new List<Integer>(0));
+
+}
